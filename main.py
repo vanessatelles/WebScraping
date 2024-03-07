ea4c875beb5da7241b412ee89d12754b9c5b9250
vanessatelles <tellesvanessa07@gmail.com>
2024-03-06 21:36:54 -0300

    Add: fetch data from api function.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import requests
 
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
+
+def fetch_data_from_api(url):
+  """
+  Method to call the API and fetch data from the endpoint.    
+  Returns:
+      dict: Data received from the API as a JSON dictionary.
+  """
+  try:
+    response = requests.get(url, headers= HEADERS, timeout= 20)
+    return response.text
+  except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")   
 
 if __name__ == '__main__':
+    
     print("start")
