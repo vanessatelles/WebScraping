e6b89799f2bc07306bc1a4e68d8d1d8eab14c4b4
vanessatelles <tellesvanessa07@gmail.com>
2024-03-11 22:08:59 -0300

    Add: moving_average_values method.

--- main.py
+++ main.py
@@ -54,7 +54,32 @@ def get_previous_closeValue(symbols):
 
   return previous_close_values
 
+def moving_average(symbols):
+  """
+  Feacth data from the API to each symbol.
+  Parse data from html into a beautifulsoup object.
+  Scrap the content for the Stock 200-Day Moving Average value.
+  Returns:
+    moving_average_values(list): A list with the moving average value to each stock.
+  """
+  moving_average_values = []
+
+  for symbol in symbols:
+    
+    response = fetch_data_from_api(f'https://finance.yahoo.com/quote/{symbol}/key-statistics?p={symbol}')
+    page  = BeautifulSoup(response, 'html.parser')
+    two_columns = page.find('div',{'id':'Col1-0-KeyStatistics-Proxy'})
+    right_column = two_columns.find('div', {'class':'Fl(end) W(50%) smartphone_W(100%)'})
+    td = right_column.find_all('td',{'class':'Fw(500) Ta(end) Pstart(10px) Miw(60px)'})
+    moving_average_value = td[6]
+    moving_average_values.append(float(moving_average_value.text))
+
+  print("Got all moving average values.")
+
+  return moving_average_values
+
 if __name__ == '__main__':
 
     ticker_values = get_symbols()
     previous_close_values = get_previous_closeValue(ticker_values)
+    moving_average_values = moving_average(ticker_values)
