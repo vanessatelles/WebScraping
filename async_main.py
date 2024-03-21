d06a33d97dcdfe0d725f748a54764605906d2f31
vanessatelles <tellesvanessa07@gmail.com>
2024-03-20 22:26:16 -0300

    Add: get_symbols method.

--- async_main.py
+++ async_main.py
@@ -1,8 +1,8 @@
 import aiohttp
 import asyncio
+from bs4 import BeautifulSoup
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-
 
 async def fetch_data_from_api(url):
 
@@ -17,6 +17,18 @@ async def fetch_data_from_api(url):
         except asyncio.TimeoutError:
             # Handle a timeout error here
             print("Request timed out.")
+
+async def get_symbols():
+
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    response = await fetch_data_from_api(url)
+    # parse data from the html into a beautifulsoup object
+    soup = BeautifulSoup(response, 'html.parser')
+    companies_table=soup.find('table',{'id':'constituents','class':"wikitable sortable"})
+    a = companies_table.find_all('a',{'class':"external text"})
+    symbols =  [symbol.text.strip() for symbol in a[:50]]        
+
+    return symbols
 
 if __name__ == '__main__':
     print("start")
